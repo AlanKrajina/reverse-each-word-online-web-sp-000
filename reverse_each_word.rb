fe45dcf8505a1ac823b1a4119fb84d3614c5c894
Alan Krajina <alankrajina88@gmail.com>
2019-05-10 20:29:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,14 +7,6 @@ def reverse_each_word(sentence1)
   return reversed.join(" ")
 end
 
-def reverse_each_word(sentence2)
-  sentence=sentence1.split(" ")
-  reversed=[]
-  sentence.collect do |i| 
-  reversed << i.reverse
-  end
-  #return reversed.join(" ")
 
-end
 
 #samo svaku rijec zasebno okrenut
