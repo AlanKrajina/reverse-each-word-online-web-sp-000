b5b8add76f88f51ca322778f5b6a70b8e4b9c754
Alan Krajina <alankrajina88@gmail.com>
2019-05-10 20:32:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,9 +9,11 @@ end
 
     
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
-end
-  reversed.join(" ")
+  sentence=sentence1.split(" ")
+  reversed=[]
+  sentence.collect do |i| 
+  reversed << i.reverse
+  end
+  return reversed.join(" ")
 end
 
